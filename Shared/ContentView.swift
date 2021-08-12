da408125be6777dde0c2b9b6aea01e8c640af171
Václav Brož <vaclav.broz@icloud.com>
2021-08-12 13:16:16 +0200

    Create Xcode project

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Václav Brož on 12.A.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
