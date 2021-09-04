db50e0a576928693369fb52a8d3e80d1accbaf8e
Václav Brož <vaclav.broz@icloud.com>
2021-09-04 18:58:13 +0200

    Create MoyaProvider request method using new concurrency

---
+++ Sources/Extensions/MoyaProvider.swift
@@ -0,0 +1,14 @@
+//  MoyaProvider.swift
+//  Created by Václav Brož on 4/9/2021
+
+import Moya
+
+extension MoyaProvider {
+    func request(_ target: Target) async throws -> Response  {
+        try await withCheckedThrowingContinuation { continuation in
+            request(target) { result in
+                continuation.resume(with: result)
+            }
+        }
+    }
+}
