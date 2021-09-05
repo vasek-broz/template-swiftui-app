380dacca1be26d425dffc6325fa26db966d6be7c
Václav Brož <vaclav.broz@icloud.com>
2021-09-05 10:11:27 +0200

    Change logic distribution in TemplateSwiftUIApp

--- Sources/Utilities/TemplateSwiftUIApp.swift
+++ Sources/Utilities/TemplateSwiftUIApp.swift
@@ -6,31 +6,17 @@ import Swinject
 
 @main
 struct TemplateSwiftUIApp: App {
-    // MARK: - Properties -
-    let assembler = Assembler()
-    let appState: AppState
-    let routing: Routing
-    
-    // MARK: - Body -
     var body: some Scene {
         WindowGroup {
             getRootView()
-                .environmentObject(appState)
-                .environmentObject(routing)
         }
     }
     
-    
-    // MARK: - Initialiazer -
-    init() {
-        assembler.apply(assembly: AppAssembly())
-        appState = assembler.resolver.resolve(AppState.self)!
-        routing = assembler.resolver.resolve(Routing.self)!
-    }
-    
-    // MARK: - Methods -
-    func getRootView() -> RootView {
-        assembler.apply(assembly: RootAssembly())
-        return assembler.resolver.resolve(RootView.self, argument: assembler)!
+    func getRootView() -> some View {
+        let assembler = Assembler([AppAssembly(), RootAssembly()])
+        let rootView = assembler.resolver.resolve(RootView.self, argument: assembler)!
+        let appState = assembler.resolver.resolve(AppState.self)!
+        let routing = assembler.resolver.resolve(Routing.self)!
+        return rootView.environmentObject(appState).environmentObject(routing)
     }
 }
