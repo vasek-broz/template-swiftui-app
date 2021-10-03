564ea3dd773aa739b380982ebdf6a8ced2ce6a35
Václav Brož <vaclav.broz@icloud.com>
2021-10-03 18:40:58 +0200

    Remove Root section

--- Sources/Sections/Root/Utilities/RootProtocols.swift
+++
@@ -1,9 +0,0 @@
-//  RootProtocols.swift
-//  Created by Václav Brož on 4/9/2021
-
-import SwiftUI
-
-// MARK: - ChildViewsFactorable -
-protocol RootChildViewsFactorable {
-    func getTemplate() -> AnyView
-}
