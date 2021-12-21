c89269a479bba991cca609510e41950894b1c627
Václav Brož <vaclav.broz@icloud.com>
2021-12-21 10:18:50 +0100

    Create Targetable protocol and TemplateTarget

---
+++ Sources/Protocols/Targetable.swift
@@ -0,0 +1,6 @@
+//  Targetable.swift
+//  Created by Václav Brož on 17/12/2021
+
+import Moya
+
+protocol Targetable: TargetType, Hashable {}
---
+++ Sources/Utilities/Targets/TemplateTarget.swift
@@ -0,0 +1,50 @@
+//  TemplateTarget.swift
+//  Created by Václav Brož on 17/12/2021
+
+import Foundation
+import Moya
+
+enum TemplateTarget {
+    case template(ignoreCache: Bool)
+}
+
+extension TemplateTarget: Targetable {
+    var baseURL: URL {
+        URL(string: "https://api.template.com")!
+    }
+    
+    var path: String {
+        switch self {
+        case .template:
+            return "/template"
+        }
+    }
+    
+    var method: Moya.Method {
+        switch self {
+        case .template:
+            return .get
+        }
+    }
+    
+    var sampleData: Data {
+        switch self {
+        case .template:
+            return "{\"template\": \"template\"}".data(using: .utf8)!
+        }
+    }
+    
+    var task: Task {
+        switch self {
+        case .template:
+            return .requestPlain
+        }
+    }
+    
+    var headers: [String : String]? {
+        switch self {
+        case .template:
+            return nil
+        }
+    }
+}
