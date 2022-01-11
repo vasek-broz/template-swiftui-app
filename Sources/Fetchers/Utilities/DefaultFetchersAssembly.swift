6a77ea1d88724f7793c8401fd79c1dc6f1de2628
Václav Brož <vaclav.broz@icloud.com>
2022-01-11 10:29:16 +0100

    Create DefaultFetchersAssembly

---
+++ Sources/Fetchers/Utilities/DefaultFetchersAssembly.swift
@@ -0,0 +1,12 @@
+//  DefaultFetchersAssembly.swift
+//  Created by Václav Brož on 21/12/2021
+
+import Swinject
+
+struct DefaultFetchersAssembly: Assembly {
+    func assemble(container: Container) {        
+        container.register(TemplateFetchable.self) { resolver in
+            DefaultTemplateFetcher(provider: resolver.resolve(TargetNetworkingProvider<TemplateTarget>.self)!)
+        }
+    }
+}
